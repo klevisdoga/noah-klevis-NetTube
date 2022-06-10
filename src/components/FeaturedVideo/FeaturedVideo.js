4c650130a4e96ca87e7a912eb8ba3308f1f92090
Noah-J-Williams <noahw869@gmail.com>
2022-06-10 15:55:55 -0300

    random featured

--- src/components/FeaturedVideo/FeaturedVideo.js
+++ src/components/FeaturedVideo/FeaturedVideo.js
@@ -1,20 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PH from '../../assets/images/video-PH.jpeg'
+import axios from '../../data/axios';
+import requests from '../../data/requests';
 import './FeaturedVideo.scss'
+const image_url = "https://image.tmdb.org/t/p/original/";
 
-export default function FeaturedVideo() {
-    return (
-        <div className='featured'>
-            <h1 className='featured__title'>Featured</h1>
-            <div className='featured__movie'>
-                <img className='featured__movie-image' src={PH} alt='blah' />
-                <div className='featured__movie-text'>
-                    <span className='featured__movie-title'>{'Movie Title'}</span>
-                    <span className='featured__movie-desc'>
-                        {"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore "}
-                    </span>
+export default class FeaturedVideo extends Component {
+    state = {
+        movies: [],
+        featured: null
+    }
+
+    getRandomFeatured(array){
+        let index = Math.floor(Math.random() * array.length);
+        this.setState({featured: array[index]});
+    }
+
+    componentDidMount() {
+        axios.get(requests.fetchTrending)
+        .then(res => {
+            console.log(res);
+            this.setState({movies: res.data.results});
+            this.getRandomFeatured(this.state.movies);
+        })
+        .catch(res => console.log(res));
+    }
+
+    render() {
+        if(!this.state.featured){
+            return <div />
+        }
+
+        return (
+            <div className='featured'>
+                <h1 className='featured__title'>Featured</h1>
+                <div className='featured__movie'>
+                    <img className='featured__movie-image' src={`${image_url}${this.state.featured.backdrop_path}`} alt='blah' />
+                    <div className='featured__movie-text'>
+                        <span className='featured__movie-title'>{this.state.featured.title}</span>
+                        <span className='featured__movie-desc'>
+                            {this.state.featured.overview}
+                        </span>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -5,13 +5,20 @@ import './Row.scss'
 const image_url = "https://image.tmdb.org/t/p/original/";
 
 
+
 export default function Row({ title, movies }) {
+
+    const scroller = useRef(null)
+
+    const scrollHandler = (scrollOffset) => {
+        scroller.current.scrollLeft += scrollOffset
+    }
     
     return (
         <div className='genre__one'>
             <h2 className='genre__one-title'>{title}</h2>
-            <div className='genre__one-list'>
-            <button className='genre__one-left'type='button'>BUTTON</button>
+            <div className='genre__one-list' ref={scroller}>
+            <button onClick={()=> scrollHandler(-600)} className='genre__one-left'type='button'>{"<"}</button>
             {
                 movies?.map(movie => {
                     const item = (
@@ -21,7 +28,7 @@ export default function Row({ title, movies }) {
                     return item;
                 })
             }
-            <button className='genre__one-right'type='button'>BUTTON</button>
+            <button onClick={()=> scrollHandler(600)} className='genre__one-right'type='button'>{">"}</button>
             </div>
         </div>
     )
