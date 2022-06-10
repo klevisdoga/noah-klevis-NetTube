b3a0bd491a393bf842f023f77f0f3e37b4a3072e
Noah-J-Williams <noahw869@gmail.com>
2022-06-10 13:39:25 -0300

    ajsdflkj

--- src/components/PageGenres/PageGenres.js
+++ src/components/PageGenres/PageGenres.js
@@ -1,16 +1,62 @@
 import React, { Component } from 'react'
-import GenreOne from '../GenreOne/GenreOne'
-import GenreThree from '../GenreThree/GenreThree'
-import GenreTwo from '../GenreTwo/GenreTwo'
+import Row from '../Row/Row';
+import requests from '../../data/requests';
+import axios from '../../data/axios';
 import './PageGenres.scss'
 
 export default class PageGenres extends Component {
+  state = {
+    action: []
+  }
+
+  componentDidMount() {
+    // Fetch action movies
+    axios.get(requests.fetchAction)
+    .then(res => this.setState({action: res.data.results}))
+    .catch(res => console.log(res));
+
+    //Fetch trending movies
+    axios.get(requests.fetchTrending)
+    .then(res => this.setState({trending: res.data.results}))
+    .catch(res => console.log(res));
+
+    //Fetch top rated movies
+    axios.get(requests.fetchTopRated)
+    .then(res => this.setState({top: res.data.results}))
+    .catch(res => console.log(res));
+
+    //Fetch comedy movies
+    setTimeout(() => {
+      axios.get(requests.fetchComedy)
+    .then(res => this.setState({comedy: res.data.results}))
+    .catch(res => console.log(res));
+
+    //Fetch horror movies
+    axios.get(requests.fetchHorror)
+    .then(res => this.setState({horror: res.data.results}))
+    .catch(res => console.log(res));
+
+    // Fetch romance movies
+    axios.get(requests.fetchRomance)
+    .then(res => this.setState({romance: res.data.results}))
+    .catch(res => console.log(res));
+
+    //Fetch documentaries
+    axios.get(requests.fetchDocumentaries)
+    .then(res => this.setState({documentary: res.data.results}))
+    .catch(res => console.log(res));
+    }, 1);
+  }
+
   render() {
     return (
       <div className='genres'>
-          <GenreOne/>
-          <GenreTwo/>
-          <GenreThree/>
+          <Row title="Trending" movies={this.state.trending} />
+          <Row title="Top Rated" movies={this.state.top} />
+          <Row title="Action" movies={this.state.action} />
+          <Row title="Comedy" movies={this.state.comedy} />
+          <Row title="Horror" movies={this.state.horror} />
+          <Row title="Romance" movies={this.state.romance} />
       </div>
     )
   }
--- src/components/SingleVideo/SingleVideo.js
+++ src/components/SingleVideo/SingleVideo.js
@@ -2,12 +2,10 @@ import React from 'react'
 import pH from '../../assets/images/video-PH.jpeg';
 import './SingleVideo.scss'
 
-export default function SingleVideo() {
+export default function SingleVideo({ image }) {
   return (
     <div className='movie'>
-        <img src={pH} alt='img' className='movie__thumbnail'/>
-        <img src={pH} alt='img' className='movie__thumbnail'/>
-        <img src={pH} alt='img' className='movie__thumbnail'/>
+        <img src={image} alt='img' className='movie__thumbnail'/>
     </div>
   )
 }
